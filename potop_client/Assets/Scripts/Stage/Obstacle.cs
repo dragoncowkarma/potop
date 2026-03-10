28fe182ce4eadcf28678ae180aced5ab5aea2895
mgkwon <kmg0512@gmail.com>
2026-03-10 21:08:54 +0900

    오브젝트 생성

---
+++ potop_client/Assets/Scripts/Stage/Obstacle.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour {
+    public Rigidbody rb;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        rb.AddForce(-transform.position);
+    }
+}
---
+++ potop_client/Assets/Scripts/_Main/StageMain.cs
@@ -0,0 +1,28 @@
+using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class StageMain : MonoBehaviour {
+    public static StageMain main;
+    public GameObject obstacle;
+    private float t = 0f;
+    
+    private void Awake() {
+        main = this;
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        t += Time.deltaTime;
+        if (t > Random.Range(3f, 5f)) {
+            t = 0;
+            var a = Random.Range(0f, 360f);
+            Instantiate(obstacle, new Vector3(10 * Mathf.Sin(a), 10f, 10 * Mathf.Cos(a)), Quaternion.identity);
+        }
+    }
+}
